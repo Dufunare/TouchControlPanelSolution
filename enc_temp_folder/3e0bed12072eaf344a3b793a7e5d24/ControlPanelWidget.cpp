0d6f401d511ca644d5a1a7dde694e15da6eb97ee
Duchenyu <1525139014@qq.com>
2026-04-18 23:34:21 +0800

    comment:add comment and adjust code block for beatification

--- enc_temp_folder/3e0bed12072eaf344a3b793a7e5d24/ControlPanelWidget.cpp
+++ enc_temp_folder/3e0bed12072eaf344a3b793a7e5d24/ControlPanelWidget.cpp
@@ -14,12 +14,10 @@
 ControlPanelWidget::ControlPanelWidget(QWidget *parent)
     : QWidget(parent)
 {
-    //根布局
     auto *rootLayout = new QVBoxLayout(this);
     rootLayout->setContentsMargins(12, 12, 12, 12);
     rootLayout->setSpacing(12);
 
-    //touch控制区域
     auto *touchGroup = new QGroupBox("Touch 控制", this);
     auto *touchLayout = new QVBoxLayout(touchGroup);
 
@@ -33,7 +31,6 @@ ControlPanelWidget::ControlPanelWidget(QWidget *parent)
     touchLayout->addWidget(m_stopButton);
     touchLayout->addWidget(m_resetButton);
 
-    //中转站控制区域
     auto *TCPGroup = new QGroupBox("中转站控制", this);
     auto *TCPLayout = new QVBoxLayout(TCPGroup);
 
@@ -54,42 +51,6 @@ ControlPanelWidget::ControlPanelWidget(QWidget *parent)
     TCPLayout->addWidget(m_robotConnectButton);
     TCPLayout->addWidget(m_robotDisconnectButton);
 
-    //机械臂控制区
-    QGroupBox* robotGroup = new QGroupBox("机械臂控制");
-    QVBoxLayout* mainVLayout = new QVBoxLayout(robotGroup);
-
-    m_emergencyStopButton = new QPushButton("急停");
-    m_powerOnButton = new QPushButton("上电", robotGroup);
-    m_enableRobotButton = new QPushButton("使能", robotGroup);
-    m_disableRobotButton = new QPushButton("下使能", robotGroup);
-    m_clearErrorButton = new QPushButton("清除错误码", robotGroup);
-    m_startDragButton = new QPushButton("启动拖拽", robotGroup);
-    m_stopDragButton = new QPushButton("停止拖拽", robotGroup);
-    m_startTeleopButton = new QPushButton("开始控制（按住按钮跟随）", robotGroup);
-    m_stopTeleopButton = new QPushButton("停止控制", robotGroup);
-
-    m_emergencyStopButton->setStyleSheet("background-color: red; color: white; font-weight: bold; height: 50px;");
-    mainVLayout->addWidget(m_emergencyStopButton);
-    mainVLayout->addSpacing(10);
-
-    QGridLayout* stateGridLayout = new QGridLayout();
-    stateGridLayout->addWidget(m_powerOnButton, 0, 0);
-    stateGridLayout->addWidget(m_enableRobotButton, 1, 0);
-    stateGridLayout->addWidget(m_disableRobotButton, 1, 1);
-    stateGridLayout->addWidget(m_clearErrorButton, 0, 1);
-    mainVLayout->addLayout(stateGridLayout);
-
-    QHBoxLayout* modeHLayout = new QHBoxLayout();
-    modeHLayout->addWidget(m_startDragButton);
-    modeHLayout->addWidget(m_stopDragButton); 
-    mainVLayout->addLayout(modeHLayout);
-
-    QVBoxLayout* teleopLayout = new QVBoxLayout();
-    teleopLayout->addWidget(m_startTeleopButton);
-    teleopLayout->addWidget(m_stopTeleopButton);
-    mainVLayout->addLayout(teleopLayout);
-
-    //视频控制区
     auto* videoGroup = new QGroupBox("视频控制", this);
     auto* videoRootLayout = new QVBoxLayout(videoGroup);
     auto* videoFormLayout = new QFormLayout();
@@ -126,15 +87,57 @@ ControlPanelWidget::ControlPanelWidget(QWidget *parent)
     videoButtonLayout->addWidget(m_videoStopButton);
     videoButtonLayout->addWidget(m_videoSnapshotButton);
     videoRootLayout->addLayout(videoButtonLayout);
+
+
     
-    //加入跟布局
+QGroupBox *robotGroup = new QGroupBox("机械臂控制");
+QVBoxLayout *mainVLayout = new QVBoxLayout(robotGroup);
+m_emergencyStopButton = new QPushButton("急停");
+m_emergencyStopButton->setStyleSheet("background-color: red; color: white; font-weight: bold; height: 50px;");
+mainVLayout->addWidget(m_emergencyStopButton);
+mainVLayout->addSpacing(10); 
+
+
+    m_powerOnButton = new QPushButton("上电", robotGroup);
+    m_enableRobotButton = new QPushButton("使能", robotGroup);
+    m_disableRobotButton = new QPushButton("下使能", robotGroup);
+    m_clearErrorButton = new QPushButton("清除错误码", robotGroup);
+
+QGridLayout *stateGridLayout = new QGridLayout();
+stateGridLayout->addWidget(m_powerOnButton, 0, 0);
+stateGridLayout->addWidget(m_enableRobotButton, 1, 0);
+stateGridLayout->addWidget(m_disableRobotButton, 1, 1);
+stateGridLayout->addWidget(m_clearErrorButton, 0, 1);
+mainVLayout->addLayout(stateGridLayout); // 将网格布局塞入主垂直布局
+
+// 4. 模式控制 (水平布局)
+    m_startDragButton = new QPushButton("启动拖拽", robotGroup);
+    m_stopDragButton = new QPushButton("停止拖拽", robotGroup);
+QHBoxLayout *modeHLayout = new QHBoxLayout();
+modeHLayout->addWidget(m_startDragButton);
+modeHLayout->addWidget(m_stopDragButton); // 把按钮挤到左边，或者填满其他模式按钮
+mainVLayout->addLayout(modeHLayout);
+
+
+m_startTeleopButton = new QPushButton("开始控制（按住按钮跟随）", robotGroup);
+m_stopTeleopButton = new QPushButton("停止控制", robotGroup);
+QVBoxLayout* teleopLayout = new QVBoxLayout();
+teleopLayout->addWidget(m_startTeleopButton);
+teleopLayout->addWidget(m_stopTeleopButton);
+mainVLayout->addLayout(teleopLayout);
+
+
+
+
+
+
+
     rootLayout->addWidget(touchGroup);
     rootLayout->addWidget(TCPGroup);
     rootLayout->addWidget(videoGroup);
     rootLayout->addWidget(robotGroup);
     rootLayout->addStretch(1);
 
-    //信号与槽连线
     connect(m_initializeButton, &QPushButton::clicked, this, &ControlPanelWidget::initializeRequested);
     connect(m_startButton, &QPushButton::clicked, this, &ControlPanelWidget::startRequested);
     connect(m_stopButton, &QPushButton::clicked, this, &ControlPanelWidget::stopRequested);
